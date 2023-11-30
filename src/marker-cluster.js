3b8a3e47ec99216bd1fea744a70cf15afd968e8e
DESKTOP-UKOD40C\Matt_ <hartmann.matth@gmail.com>
2023-11-30 15:39:21 +0100

    Ajout api google map

--- src/marker-cluster.js
+++ src/marker-cluster.js
@@ -1,6 +1,6 @@
 const loadGoogleMaps = () => {
   const script = document.createElement("script");
-  script.src = "https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&callback=initMap";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDCgdVCDyuzjqS2rv8Hs3EZehY4QE-XL4g&callback=initMap";
   script.async = true;
   document.head.appendChild(script);
 };
